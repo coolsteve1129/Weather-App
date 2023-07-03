a9e82a0db3a15a40918213d503a5a66d22c1a8e9
coolsteve1129 <hecksteven2@gmail.com>
2023-07-03 15:37:19 -0500

    Updated using api and added css

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const locationDiv = document.getElementById("location")
 const tempDiv = document.getElementById("temp")
 const cdnImg = document.getElementById("cdnImg")
 const cdnDesc = document.getElementById("cdnDesc")
+const windSpd = document.getElementById("windSpd")
+const windDir = document.getElementById("windDir")
+const feelsLike = document.getElementById("feelsLike")
 
 
 searchBtn.addEventListener("click", function(event) {  
@@ -20,18 +23,43 @@ async function searchLocation(userInput) {
         }
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=887a9b8447e44be98b4224831232506&q=${userInput}`, {mode: 'cors'});
       const searchedLocation = await response.json()
-      locationDiv.innerHTML = searchedLocation.location.name.toString()
+      locationDiv.innerHTML = searchedLocation.location.name +", " +searchedLocation.location.region +", " +searchedLocation.location.country
       tempDiv.innerHTML = searchedLocation.current.temp_f.toString() + "&deg;"
-      //console.log(searchedLocation.current.condition.icon)
       cdnImg.src = "https:" + searchedLocation.current.condition.icon.toString()
-      //const imgURL = "https:" + searchedLocation.current.condition.icon.toString()
-      //cdnImg.src = imgURL;
+      windSpd.innerHTML = searchedLocation.current.wind_mph +"/mph"
+      windDir.innerHTML = searchedLocation.current.wind_dir
+      feelsLike.innerHTML = searchedLocation.current.feelslike_f + "&deg;"
       cdnImg.onload = async function() {
-      cdnDesc.innerHTML = await searchedLocation.current.condition.text.toString();
+      const weatherDesc = await searchedLocation.current.condition.text.toString() 
+      cdnDesc.innerHTML = weatherDesc
+      setWeatherBackground(weatherDesc);
     };
   }  
   catch (error){
     alert("There was no picture returned. Error: "+error)
   }
+}
+function setWeatherBackground(weatherDescription) {
+  const body = document.querySelector('body');
+  const weatherClass = getWeatherClass(weatherDescription);
+
+  body.className = weatherClass;
+}
+
+function getWeatherClass(weatherDescription) {
+  const conditionMapping = {
+    'Clear': 'weather-clear',
+    'Partly cloudy': 'weather-cloudy',
+    'Cloudy': 'weather-cloudy',
+    'Light rain': 'weather-rainy',
+    'Moderate rain': 'weather-rainy',
+    'Heavy rain': 'weather-rainy',
+    'Light snow': 'weather-snowy',
+    'Moderate snow': 'weather-snowy',
+    'Heavy snow': 'weather-snowy',
+    // Include other relevant weather descriptions and corresponding weather classes
+  };
+
+  return conditionMapping[weatherDescription] || '';
 }
   searchLocation()
