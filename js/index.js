c32463f4eb6b48826708e47155c4d0574d9e5dd1
coolsteve1129 <hecksteven2@gmail.com>
2023-07-03 20:34:42 -0500

    Made it so the background image loads first

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ const cdnDesc = document.getElementById("cdnDesc")
 const windSpd = document.getElementById("windSpd")
 const windDir = document.getElementById("windDir")
 const feelsLike = document.getElementById("feelsLike")
+const container = document.getElementById("container")
 
 
 searchBtn.addEventListener("click", function(event) {  
@@ -22,83 +23,89 @@ async function searchLocation(userInput) {
           userInput = "new york";
         }
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=887a9b8447e44be98b4224831232506&q=${userInput}`, {mode: 'cors'});
-      const searchedLocation = await response.json()
+      const searchedLocation = await response.json()   
+      const weatherDesc = searchedLocation.current.condition.text.toString()    
+      setWeatherBackground(weatherDesc);
       locationDiv.innerHTML = searchedLocation.location.name +", " +searchedLocation.location.region +", " +searchedLocation.location.country
       tempDiv.innerHTML = searchedLocation.current.temp_f.toString() + "&deg;"
       cdnImg.src = "https:" + searchedLocation.current.condition.icon.toString()
       windSpd.innerHTML = searchedLocation.current.wind_mph +"/mph"
       windDir.innerHTML = searchedLocation.current.wind_dir
       feelsLike.innerHTML = searchedLocation.current.feelslike_f + "&deg;"
-      cdnImg.onload = async function() {
-      const weatherDesc = await searchedLocation.current.condition.text.toString() 
       cdnDesc.innerHTML = weatherDesc
-      setWeatherBackground(weatherDesc);
-    };
-  }  
-  catch (error){
-    alert("There was no picture returned. Error: "+error)
-  }
+    }  
+    catch (error){
+      alert("There was no picture returned. Error: "+error)
+    }
 }
 function setWeatherBackground(weatherDescription) {
-  const body = document.querySelector('body');
+  const body = document.querySelector("body");
   const weatherClass = getWeatherClass(weatherDescription);
+  const backgroundImage = new Image();
 
-  body.className = weatherClass;
+  backgroundImage.onload = function() {
+    body.style.backgroundImage = `url(./images/${weatherClass}.jpg)`;
+    container.style.display = "block";
+  };
+
+  backgroundImage.src = `./images/${weatherClass}.jpg`;
 }
 
 function getWeatherClass(weatherDescription) {
   const conditionMapping = {
-    'Sunny': 'weather-sunny',
-  'Clear': 'weather-sunny',
-  'Partly cloudy': 'weather-cloudy',
-  'Cloudy': 'weather-cloudy',
-  'Overcast': 'weather-cloudy',
-  'Mist': 'weather-cloudy',
-  'Patchy rain possible': 'weather-rainy',
-  'Patchy snow possible': 'weather-snowy',
-  'Patchy sleet possible': 'weather-snowy',
-  'Patchy freezing drizzle possible': 'weather-rainy',
-  'Thundery outbreaks possible': 'weather-stormy',
-  'Blowing snow': 'weather-snowy',
-  'Blizzard': 'weather-snowy',
-  'Fog': 'weather-cloudy',
-  'Freezing fog': 'weather-cloudy',
-  'Patchy light drizzle': 'weather-rainy',
-  'Light drizzle': 'weather-rainy',
-  'Freezing drizzle': 'weather-rainy',
-  'Heavy freezing drizzle': 'weather-rainy',
-  'Patchy light rain': 'weather-rainy',
-  'Light rain': 'weather-rainy',
-  'Moderate rain at times': 'weather-rainy',
-  'Moderate rain': 'weather-rainy',
-  'Heavy rain at times': 'weather-rainy',
-  'Heavy rain': 'weather-rainy',
-  'Light freezing rain': 'weather-rainy',
-  'Moderate or heavy freezing rain': 'weather-rainy',
-  'Light sleet': 'weather-snowy',
-  'Moderate or heavy sleet': 'weather-snowy',
-  'Patchy light snow': 'weather-snowy',
-  'Light snow': 'weather-snowy',
-  'Patchy moderate snow': 'weather-snowy',
-  'Moderate snow': 'weather-snowy',
-  'Patchy heavy snow': 'weather-snowy',
-  'Heavy snow': 'weather-snowy',
-  'Ice pellets': 'weather-snowy',
-  'Light rain shower': 'weather-rainy',
-  'Moderate or heavy rain shower': 'weather-rainy',
-  'Torrential rain shower': 'weather-rainy',
-  'Light sleet showers': 'weather-snowy',
-  'Moderate or heavy sleet showers': 'weather-snowy',
-  'Light snow showers': 'weather-snowy',
-  'Moderate or heavy snow showers': 'weather-snowy',
-  'Light showers of ice pellets': 'weather-snowy',
-  'Moderate or heavy showers of ice pellets': 'weather-snowy',
-  'Patchy light rain with thunder': 'weather-stormy',
-  'Moderate or heavy rain with thunder': 'weather-stormy',
-  'Patchy light snow with thunder': 'weather-stormy',
-  'Moderate or heavy snow with thunder': 'weather-stormy',
+    'Sunny': 'Sunny',
+  'Clear': 'Sunny',
+  'Partly cloudy': 'Cloudy',
+  'Cloudy': 'Cloudy',
+  'Overcast': 'Cloudy',
+  'Mist': 'Cloudy',
+  'Patchy rain possible': 'Rainy',
+  'Patchy snow possible': 'Snowy',
+  'Patchy sleet possible': 'Snowy',
+  'Patchy freezing drizzle possible': 'Rainy',
+  'Thundery outbreaks possible': 'Stormy',
+  'Blowing snow': 'Snowy',
+  'Blizzard': 'Snowy',
+  'Fog': 'Cloudy',
+  'Freezing fog': 'Cloudy',
+  'Patchy light drizzle': 'Rainy',
+  'Light drizzle': 'Rainy',
+  'Freezing drizzle': 'Rainy',
+  'Heavy freezing drizzle': 'Rainy',
+  'Patchy light rain': 'Rainy',
+  'Light rain': 'Rainy',
+  'Moderate rain at times': 'Rainy',
+  'Moderate rain': 'Rainy',
+  'Heavy rain at times': 'Rainy',
+  'Heavy rain': 'Rainy',
+  'Light freezing rain': 'Rainy',
+  'Moderate or heavy freezing rain': 'Rainy',
+  'Light sleet': 'Snowy',
+  'Moderate or heavy sleet': 'Snowy',
+  'Patchy light snow': 'Snowy',
+  'Light snow': 'Snowy',
+  'Patchy moderate snow': 'Snowy',
+  'Moderate snow': 'Snowy',
+  'Patchy heavy snow': 'Snowy',
+  'Heavy snow': 'Snowy',
+  'Ice pellets': 'Snowy',
+  'Light rain shower': 'Rainy',
+  'Moderate or heavy rain shower': 'Rainy',
+  'Torrential rain shower': 'Rainy',
+  'Light sleet showers': 'Snowy',
+  'Moderate or heavy sleet showers': 'Snowy',
+  'Light snow showers': 'Snowy',
+  'Moderate or heavy snow showers': 'Snowy',
+  'Light showers of ice pellets': 'Snowy',
+  'Moderate or heavy showers of ice pellets': 'Snowy',
+  'Patchy light rain with thunder': 'Stormy',
+  'Moderate or heavy rain with thunder': 'Stormy',
+  'Patchy light snow with thunder': 'Stormy',
+  'Moderate or heavy snow with thunder': 'Stormy',
   };
 
   return conditionMapping[weatherDescription] || '';
 }
+
+
   searchLocation()
